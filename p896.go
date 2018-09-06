afe6e654b765233ab36861f07b7d71d9ddbdd254
🐶 <jimac@deiMac.local>
2018-09-06 21:12:21 +0800

    problem896

---
+++ p896.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+func isMonotonic(A []int) bool {
+	if len(A) < 2 {
+		return true
+	}
+
+	ret := 1
+	if A[len(A)-1] <= A[0] {
+		ret = 0
+	}
+
+	for i := 1; i < len(A); i++ {
+		switch ret {
+		case 1:
+			if A[i] < A[i-1] {
+				return false
+			}
+		case 0:
+			if A[i] > A[i-1] {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+func main() {
+	fmt.Println(isMonotonic([]int{4, 4, 4, 4}))
+}
