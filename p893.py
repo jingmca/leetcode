6a84bcdc959195927a0c992a762c3698b5c4c44d
🐶 <jimac@deiMac.local>
2018-09-13 02:40:03 +0800

    p893.py

---
+++ p893.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def numSpecialEquivGroups(self, A):
+        def count(A):
+            ans = [0] * 52
+            for i, letter in enumerate(A):
+                ans[ord(letter) - ord('a') + 26 * (i%2)] += 1
+            return tuple(ans)
+
+        return len({count(word) for word in A})
+
+
+s = Solution()
+print(s.numSpecialEquivGroups(["abcd","cdab","adcb","cbad"]))
