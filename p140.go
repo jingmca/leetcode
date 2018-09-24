41d34264b6f61cb034d4458c5717576b2916bc59
🐶 <jimac@deiMac.local>
2018-09-25 00:43:34 +0800

    140 ***

--- p140.go
+++ p140.go
@@ -7,84 +7,64 @@ import (
 
 func wordBreak(s string, wordDict []string) []string {
 
-	endpoints := make(map[int][]int)
+	if len(s) == 0 {
+		return []string{}
+	}
 
+	alphabets := make(map[rune]bool)
 	for _, w := range wordDict {
-
-		for i, width := 0, len(w); strings.Contains(s[i:], w); {
-			idx := strings.Index(s[i:], w)
-			if idx > -1 {
-				endpoints[idx+i] = append(endpoints[idx+i], idx+i+width)
-				i += idx + 1
-			}
+		for _, c := range w {
+			alphabets[c] = true
 		}
 	}
 
-	stop := len(s)
-
-	del := make(map[int][]int)
-	for k, v := range endpoints {
-		for _, iv := range v {
-			if _, ok := endpoints[iv]; !ok && iv != stop {
-				del[k] = append(del[k], iv)
-			}
+	for _, c := range s {
+		if _, ok := alphabets[c]; !ok {
+			return []string{}
 		}
 	}
 
-	fmt.Println(del)
-
-	for k, v := range del {
-		for _, iv := range v {
-			for idx, iiv := range endpoints[k] {
-				if iiv == iv {
-					endpoints[k] = append(endpoints[k][:idx], endpoints[k][idx+1:]...)
-				}
-			}
-		}
-	}
-
-	fmt.Println(endpoints)
-
-	res := make([]string, 0)
-
-	calc(0, stop, []int{}, endpoints, &res, s)
-
-	// fmt.Println(output)
-
-	// for _, o := range output {
-	// 	_str := make([]string, 0, len(o)-1)
-	// 	for i := 0; i < len(o)-1; i++ {
-	// 		_str = append(_str, s[o[i]:o[i+1]])
-	// 	}
-	// 	fmt.Println(strings.Join(_str, " "), o)
-	// 	res = append(res, strings.Join(_str, " "))
-	// }
-
-	return res
+	var record = make(map[string][]string)
+	return wordBreak2(s, wordDict, &record)
 }
 
-func calc(start int, stop int, prev []int, ends map[int][]int, res *[]string, s string) {
-	if next, ok := ends[start]; ok {
-		for _, idx := range next {
-			if idx == stop {
-				_out := append(prev, start, idx)
-				// *out = append(*out, _out)
-				// fmt.Printf("%v, %d, %d, %v\n", _out, idx, stop, out)
-				_str := make([]string, 0, len(_out)-1)
-				for i := 0; i < len(_out)-1; i++ {
-					_str = append(_str, s[_out[i]:_out[i+1]])
-				}
-				// fmt.Println(strings.Join(_str, " "), _out)
-				*res = append(*res, strings.Join(_str, " "))
+func wordBreak2(s string, wordDict []string, rDict *map[string][]string) []string {
+	res := make([]string, 0)
 
+	for _, w := range wordDict {
+		head := strings.Index(s, w)
+
+		if head == 0 {
+			_s := s[len(w):]
+			if len(_s) == 0 {
+				res = append(res, w)
 			} else {
-				path := append(prev, start)
-				calc(idx, stop, path, ends, res, s)
+				if _r, ok := (*rDict)[_s]; !ok {
+					_r = wordBreak2(_s, wordDict, rDict)
+					for _, rs := range _r {
+						res = append(res, fmt.Sprintf("%s %s", w, rs))
+					}
+
+				} else {
+					for _, rs := range _r {
+						res = append(res, fmt.Sprintf("%s %s", w, rs))
+					}
+				}
+
 			}
+
 		}
 	}
+	(*rDict)[s] = res
+	return res
 }
 
 func main() {
 	fmt.Println(wordBreak("aaaaaaa", []string{"aaaa", "aaa"}))
+
+	// fmt.Println(wordBreak("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", []string{"aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa", "ba"}))
+	fmt.Println(wordBreak("catsanddog", []string{"cat", "cats", "and", "sand", "dog"}))
+	fmt.Println(wordBreak("pineapplepenapple", []string{"apple", "pen", "applepen", "pine", "pineapple"}))
+	fmt.Println(wordBreak("catsandog", []string{"cats", "dog", "sand", "and", "cat"}))
+
 }
